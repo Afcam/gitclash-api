7316778b49e0b52b120424d139dce0454d717483
AFCampos <afcampos.dev@gmail.com>
2023-06-19 12:06:01 -0400

    feat: add login with jwt token

--- src/controllers/rooms-controller.js
+++ src/controllers/rooms-controller.js
@@ -1,8 +1,25 @@
 const knex = require('knex')(require('../../knexfile'));
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
+  const { room_id } = req.body;
+
+  if (!room_id) {
+    return res.status(400).json({
+      message: 'Login requires a room id',
+    });
+  }
+
   try {
-    res.status(200).json('nice');
+    const room = await knex('rooms').where({ id: room_id });
+
+    if (room.length === 0) {
+      return res.status(400).send('Room ID does not exist');
+    }
+
+    const token = jwt.sign({ roomId: room_id }, process.env.JWT_SECRET);
+
+    res.status(201).json({ token });
   } catch (error) {
     console.error(error);
   }
